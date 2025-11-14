7733a06c43d7e66a58abf06b4e958d68488252c0
Sabbir Hossain <sm.sabbir9671@gmail.com>
2025-11-15 00:44:43 +0600

    API added to the card container

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-//Categories section start here
 const categoryContainer = document.getElementById("categoriesContainer");
+const treesByContainer = document.getElementById("treesByCategories");
 
 const showCategory = (categories) => {
   categories.forEach((cat) => {
@@ -9,22 +9,45 @@ const showCategory = (categories) => {
             `;
   });
   categoryContainer.addEventListener("click", (e) => {
-    const allLi = document.querySelectorAll('li');
-    allLi.forEach(li =>{
-        li.classList.remove("bg-green-700", "text-white")
-    })
+    const allLi = document.querySelectorAll("li");
+    allLi.forEach((li) => {
+      li.classList.remove("bg-green-700", "text-white");
+    });
     // console.log(e);
     if (e.target.localName === "li") {
-      console.log(e.target)
-      e.target.classList.add("bg-green-700", "text-white")
+      //   console.log(e.target.id)
+      e.target.classList.add("bg-green-700", "text-white");
+      loadTreesByCategory(e.target.id);
     }
   });
 };
-const loadTreeCategory = () => {
+const showTreesByCategory = (plants) => {
+  console.log(plants);
+    plants.forEach(plant => {
+        treesByContainer.innerHTML += `
+        <div class="border border-orange-500 p-4">
+                        <img class="" src="${plant.image}" alt="">
+                        <h1 class="text-[16px] font-semibold mt-1">${plant.name}</h1>
+                        <p class="text-[12px]">${plant.description}</p>
+                        <!-- Free products & price start here -->
+                        <div class="mt-2 flex justify-between">
+                            <p class="bg-green-200 px-5 py-1 font-semibold rounded-xl text-[14px] text-green-700">${plant.category}</p>
+                            <span class="font-bold">$${plant.price}</span>
+                        </div>
+                        <button class="btn btn-wide bg-green-700 text-white rounded-3xl mt-2">Add to Cart</button>
+                    </div>
+
+        `;
+    })
+
+  
+};
+
+const loadCategory = () => {
   fetch(`https://openapi.programming-hero.com/api/categories`)
     .then((res) => res.json())
     .then((data) => {
-    //   console.log(data.categories);
+      //   console.log(data.categories);
       const categories = data.categories;
       showCategory(categories);
     })
@@ -32,4 +55,16 @@ const loadTreeCategory = () => {
       console.log(err);
     });
 };
-loadTreeCategory();
+
+const loadTreesByCategory = (treesId) => {
+  // console.log(treesId);
+  fetch(`https://openapi.programming-hero.com/api/category/${treesId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.plants);
+      showTreesByCategory(data.plants);
+    });
+};
+
+loadCategory();
+loadTreesByCategory();
